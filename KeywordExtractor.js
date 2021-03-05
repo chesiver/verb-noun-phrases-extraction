b57030778b7b6008f1af738913e932341056491b
Adam Liu <yidong.liu@oracle.com>
2021-03-04 20:47:53 -0800

    init

---
+++ KeywordExtractor.js
@@ -0,0 +1,170 @@
+const natural = require('natural');
+const chunker = require('pos-chunker');
+class KeywordExtractor {
+
+    static tokenizeSentence(sentences) {
+        const tokenizer = new natural.SentenceTokenizer();
+        return tokenizer.tokenize(sentences);
+    }
+
+    static tokenizeWord(sentence) {
+        const tokenizer = new natural.WordTokenizer();
+        return tokenizer.tokenize(sentence);
+    }
+
+    static tagSentence(sentence) {
+        const language = "EN"
+        const defaultCategory = 'NN';
+        const defaultCategoryCapitalized = 'NNP';
+        const lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapitalized);
+        const ruleSetCurrent = new natural.RuleSet('CURRENT');
+        const ruleSetCondition = new natural.RuleSet('EN');
+        const tagger = new natural.BrillPOSTagger(lexicon, ruleSetCurrent, ruleSetCondition);
+        const tokenizedSentence = this.tokenizeWord(sentence);
+        const taggedWords = tagger.tag(tokenizedSentence);
+        return taggedWords;
+    }
+
+    static formatSentence(sentence) {
+        const taggedWords = this.tagSentence(sentence);
+        let formattedSentence = '';
+        for (const [_, tagged] of Object.entries(taggedWords)) {
+            for (let i = 0; i < tagged.length; i++) {
+                formattedSentence += tagged[i].token + '/' + tagged[i].tag.replace(/\$/g, '') + ' '
+            }
+        }
+        return formattedSentence.trim();
+    }
+
+    static getPossibleSubjects(chunked) {
+        let vpStart = chunked.indexOf('[VP');
+        if (vpStart === -1) {
+            return [];
+        }
+        let numParenthesis = 0;
+        let possibleSubjects = []
+        for (let i = vpStart; i < chunked.length; i++) {
+            if (chunked[i] === '[') {
+                numParenthesis++;
+            } else if (chunked[i] === ']') {
+                numParenthesis--;
+            }
+            if (numParenthesis === 0) {
+                const vp = chunked.substring(vpStart, i+1);
+                possibleSubjects.push(vp.trim());
+                vpStart = chunked.substring(i).indexOf('[VP');
+                if (vpStart === -1) {
+                    break;
+                }
+                vpStart = vpStart + i - 1
+                i = vpStart;
+                numParenthesis = 0;
+                // numClosing = 0
+            }
+        }
+        return possibleSubjects;
+    }
+
+    static formatSubject(subject) {
+        subject = subject.replace(/[\[|\]|\/]/g, '').replace(/[A-Z]/g, '').replace(/ {2,}/g, ' ').trim();
+        return subject;
+    }
+
+    static removeTags(trees) {
+        for (let i = 0; i < trees.length; i++) {
+            trees[i] = trees[i].replace(/\[[^ ]* /g, '') // remove left hand chunk eg. [VP
+                               .replace(/\/[^ ]* /g, ' ') // remove tag with space following backlash eg. /NN
+                               .replace(/\/[A-Z]*/g, '') // remove tags with no space following backlash 
+                               .replace(/\]/g, ''); // remove right closing bracket ]
+        }
+        return trees;
+    }
+
+    /**
+     * TODO: Distinguish date time grammar from <company name that is tagged as uncommon noun>.
+     *       natural library's POS tagger tags uncommon nouns and date time related words as 'N'
+     *       so there's no way to distinguish <company name that is tagged as uncommon noun>
+     *       from date time. 
+     *       eg. 'set reminder to send message to [company name that is an uncommon noun]'
+     *            and ‘set reminder to send message at 2pm’ will incorrectly return 
+     *            'send message to [company name that is an uncommon noun]' and 
+     *            'send message at 2pm' respectively. 
+     *            Expected return should be 'send message to [company name that is an uncommon noun]'
+     *            and 'send message' respectively. 
+     * @param {} sentence 
+     */
+    static extractSubject(sentence) {
+        // sentence = sentence.trim().toLowerCase();
+        let formattedSentence = this.formatSentence(sentence)
+        console.log(formattedSentence)
+        const NP = {
+            ruleType: 'tokens',
+            // pattern: '[ { tag:/DT|JJ|VBG|PRP|NN.*?/ } ]+',
+            pattern: '[ {tag:/DT/} ]? [ {tag:/JJ/} ]* [ {tag:/PRP/} ]* [ {tag:/VBG|NNP|NNS|NN.*?/} ]+',
+            // pattern: '[ {tag:/DT/} ]? [ {tag:/JJ/} ]* [ {tag:/IN/} ]* [ {tag:/PRP/} ]* [ {tag:/VBG|NNP|NNS|NN.*?/} ]+',
+            result: 'NP'
+        };
+
+        const PP = {
+            ruleType: 'tokens',
+            pattern: '[ { tag:IN|TO } ] [ { chunk:NP } ]+',
+            result: 'PP'
+        };
+
+        const PPN = {
+            ruleType: 'tokens',
+            // pattern: '(?:(?= [ { chunk:/PP/ } ] )[ { chunk:/PP/ } ]|(?![ { chunk:/PP/ } ])[ { chunk:/PP/ } ])',
+            // pattern: ' [ {chunk:/PP/} ] [ {chunk:/NP/} ] [ {chunk:/PP/} ]? | [ {chunk:/NP/} ]+',
+            // pattern: ' ([ {chunk:/PP/} ]) ([ {chunk:/PP/} ]).*? ',
+            pattern: '[ { chunk:/PP/ } ] ((?!=[ { chunk:/PP/ } ])|(?![ { chunk:/PP/ } ]))',
+            // (?:\D*(\d+)){2}
+
+            result: 'PPN'
+        }
+
+        // [ { chunk:/NP/} ]
+        // [ { chunk:/PPN/} ]
+
+        const VP = {
+            ruleType: 'tokens',
+            // pattern: '[ { tag:/VB.?/ } ] [ { chunk:/NP|PPN/ } ]+',
+            pattern: ' [ { tag:/VB.?/ } ] ([ { chunk:/PPN/} ][ { chunk:/NP/} ])* [ { chunk:/NP/} ]* [ { chunk:/NP|PPN/ } ]',
+            // pattern: '[ { tag:/VB.?/ } ] [ { chunk:/PPN/} ]? [ { chunk:/NP/} ]+ [ { chunk:/PPN/} ]?',
+            // pattern: '[ { tag:/VB.?/ } ] [ { chunk:/PP/ } ]' ,
+            // [ { word:have } ] (?=[ { word:/dinner|lunch/ } ])
+            // pattern: '[ { tag:/VB.?/ } ] [ { chunk:/PP/ } ] (?!=[ { chunk:/PP/ } ]) [ { chunk:/NP/ } ]+ [ { chunk:/PP/ } ]? (?=[ { chunk:/PP/ } ])',
+            // pattern: '[ {chunk:/PP/} ] | [ {chunk:/PP/} ] (?![{ chunk:/PP/ } ])',
+            // pattern: '[ { tag:/VB.?/ } ] [ { chunk:/PP/ } ] [ { chunk:/NP/ } ]  +', 
+            result: 'VP'
+        };
+
+        // grammar = "NP: {<VB.*>?<RB>?<DT>?<PRP.*>?<NN.*>?<IN>?<DT>?<JJ.*>*<NN.*>*<IN.*>?<NN.*>?}"
+
+        const rules = [NP, PP, PPN, VP]
+
+        let chunked = chunker.chunk(formattedSentence, rules);
+        let longest = 0;
+        let longestIndex = -1;
+        console.log(chunked)
+        let trees = this.getPossibleSubjects(chunked);
+        trees = this.removeTags(trees);
+        return trees;
+        console.log(trees)
+        for(let i = 0; i < trees.length; i++) {
+            if (trees[i].trim().includes('[VP')) {
+                if (trees[i].split(' ').length >= longest ) {
+                    longest = trees[i].split(' ').length;
+                    longestIndex = i;
+                }
+            }
+        }
+        if (longestIndex === -1) {
+            return '';
+        }
+
+
+        formattedSentence = trees[longestIndex]
+        return this.formatSubject(formattedSentence);
+    }
+}
+module.exports = KeywordExtractor;
---
+++ test.js
@@ -0,0 +1,60 @@
+const KeywordExtractor = require('./KeywordExtractor')
+
+// var natural = require('natural');
+// var tokenizer = new natural.SentenceTokenizer();
+// console.log(tokenizer.tokenize('remind me buy eggs next monday'));
+
+let sentence = 'create a task to make a proposal at 1pm tomorrow';
+// sentence = 'get tasks with the subject organize a meeting with pinnacle'
+// sentence = "get tasks with the subject organize pinnacle's planning meeting with pinnacle"
+// sentence = 'get task where the subject is send john his forms'
+// sentence = 'create a reminder to buy eggs next monday';
+// sentence = 'get task called follow up with pinnacle' // not work 
+// sentence = 'create reminder to call john at 2pm next week'
+// sentence = 'create the task send the note to pinnacle'
+// sentence = 'Show the task with the subject organize a meeting with pinnacle'
+sentence = 'show the task details where organizing a meeting with pinnacle systems is the subject and london is the location'
+// sentence = 'create reminder to call amy'; // amy is 'N'
+// sentence = 'create task due on 9/10/2020 called draw up proposal' //proposal is JJ
+// sentence = 'get me the details for the task buy myself eggs' // not work... how to break ties? 
+sentence = 'set a reminder to call the guy from pyramid systems at 3:30pm'
+// sentence = 'create task to create proposal for pyramid llc'
+// sentence = 'create task to buy ticket for Pinnacle as a gift and set a reminder to call the guy from pyramid systems at 3:30pm'
+// sentence = 'create task to buy ticket for Pinnacle as a gift'
+// sentence  = 'send for a demo for a demo'
+// sentence = 'create a task to call Habitat for Humanity'
+// sentence = 'create a task to send many pins'
+// sentence = 'create a task with the subject send for a demo';
+// sentence = 'create a task with send for a demo as the subject';
+// sentence = 'for a demo for a demo'
+// sentence = 'create reminder to call pinnacle next week';
+// sentence = 'create a task with the subject as send for a demo';
+// sentence = 'create task to ask Bob about his plans'
+// sentence = 'create task ask pinnacle to fly to Seattle'
+// sentence = 'get reminder with the subject meet with pinnacle'
+// sentence = 'create a reminder that is called buy eggs at 5pm'
+
+// sentence = 'create a task with the subject, plan for a demo';
+// sentence = 'create a proposal to invoice for a demo 3pm';
+// sentence = 'create a task with the subject send for a demo as the subject'
+// sentence = 'create a task with plan for a demo as the subject'
+// sentence = 'create task to buy ticket for Pinnacle as a gift and set a reminder to call the guy from pyramid systems at 3:30pm'
+// sentence = 'create a task to call Bob at 3 pm'
+// sentence = 'create task called invoice note'
+// sentence = 'create a task to plan for a demo at 2pm tomorrow'
+// sentence = 'create a task to plan for a demo at 2pm'
+// sentence = 'create a reminder to call Bob at 2 pm'
+// sentence = 'create a task to invoice the orders'
+// sentence = 'create a task to review the proposal'
+// sentence = 'create a task to call Habitat for Humanity'
+// sentence = 'create a task called buy eggs'
+// sentence = 'create a task called plan for a demo for a demo for a demo'
+// // sentence = 'amy'
+// sentence = "discussed with tom about the open SRS for the seaman's account"
+// sentence = "as the next steps, call support to escalate the critical SRS"
+// sentence = 'create a task to plan for a demo next week'
+// sentence = 'create a task to plan for a demo'
+sentence = 'Asked her for time next week for design call w/ Jason and got apptmt for next Thurs at 11 am our time'
+
+let a = KeywordExtractor.extractSubject(sentence);
+console.log(a);
