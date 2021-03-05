00a423013dafe29cd8711b3d3bbb4df7a8d2ec40
Adam Liu <yidong.liu@oracle.com>
2021-03-05 01:31:59 -0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const KeywordExtractor = require('../KeywordExtractor')
 const testData = require('./test-pairs.json')
 
-describe('Extraction Results Verification: ', () => {
+describe('Extraction Results Verification: \n', () => {
     testData.forEach(({sentence, expected}, i) => {
         it(`Example: ${sentence}\n`, () => {
             assert.deepStrictEqual(KeywordExtractor.extractSubject(sentence), expected);
