11e5234bcb2684dc0e68e09f6d299a1aa435a16b
Adam Liu <ljhchesiver@gmail.com>
2021-04-08 14:20:41 +0900

    remove useless files

--- replace.sh
+++
@@ -1,2 +0,0 @@
-rm -r ./node_modules/natural/lib/natural/brill_pos_tagger/
-cp -r ./brill_pos_tagger ./node_modules/natural/lib/natural/
--- restore.sh
+++
@@ -1,2 +0,0 @@
-rm -r ./node_modules/natural/lib/natural/brill_pos_tagger/
-cp -r ./original_brill_pos_tagger ./node_modules/natural/lib/natural/brill_pos_tagger
